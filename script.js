1634c1314e591d08b3919aa18305c73a04020862
ALKEMIA-CHARLES <charlesmtawalijr@gmail.com>
2024-04-04 09:34:13 +0300

    finish

--- script.js
+++ script.js
@@ -4,13 +4,13 @@ const searchInput = document.querySelector("[data-search]")
 
 let users = []
 
-searchInput.addEventListener("input",  e => {
+searchInput.addEventListener("input", e => {
     const value = e.target.value.toLowerCase()
     users.forEach(user => {
         const isVisible = 
             user.name.toLowerCase().includes(value) ||
             user.email.toLowerCase().includes(value)
-        user.element.classList.toggle("hide", isVisible)
+        user.element.classList.toggle("hide", !isVisible)
     })
 })
 
